fbde49b5dd8554b9500ee3b7caf70835c611fce2
Natalia <89256017432@bk.ru>
2022-12-15 19:22:34 +0300

    Lesson 2_1

--- 2_lesson/1.0/Program.cs
+++ 2_lesson/1.0/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+
+string NumSec(int num)
+{
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+      return $"{num / 10 % 10};
+    return "The number is not three-digital";
+}
+Console.WriteLine(NumSec(int.Parse(Console.ReadLine())));
+
