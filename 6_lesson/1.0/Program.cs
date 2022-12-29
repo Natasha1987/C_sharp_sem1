4fa8f14cb3560135c883757d261d91e76839ec4f
Natalia <89256017432@bk.ru>
2022-12-29 17:19:31 +0300

    Task_1

--- 6_lesson/1.0/Program.cs
+++ 6_lesson/1.0/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223->3 
+
+int Countnum()
+{
+    int count = 0;
+    String word;
+
+    while (true)
+    {
+        Console.Write("Output any number: ");
+        word = Console.ReadLine();
+
+        if (word == "") return count;
+        else if (int.Parse(word) >0) count += 1;
+    }
+}
+
+Console.Write(Countnum());
